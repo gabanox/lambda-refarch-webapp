709b3bf3aca57d5ec2bf38a1696f33587cdebac1
Gabriel <gabriel@mbp-de-gabriel.lan>
2022-02-27 09:41:57 -0600

    config

--- www/src/config.js
+++ www/src/config.js
@@ -5,7 +5,7 @@ const config = {
   "aws_user_pools_web_client_id": "3t8op37c10t1n1nkcqqedi73hb",     // CognitoClientID
   "api_base_url": "https://7ifpw1ngai.execute-api.us-east-1.amazonaws.com/prod",                                     // TodoFunctionApi
   "cognito_hosted_domain": "mytodoappdemo-api.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
-  "redirect_url": "https://master.d19jj2n3gsqr4l.amplifyapp.com"                                      // AmplifyURL
+  "redirect_url": "http://127.0.0.1:8080"                                      // AmplifyURL
 };
 
 export default config;
